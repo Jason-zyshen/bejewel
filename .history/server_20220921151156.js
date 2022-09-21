1edbfbea58d7237ec78e95fa1ff57bf2f87e38da
zyShen <80087549+Jason-zyshen@users.noreply.github.com>
2022-09-21 15:56:41 +0000

    Set up graphql server

---
+++ .history/server_20220921151156.js
@@ -0,0 +1,46 @@
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const { ApolloServer, gql } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+const { typeDefs} = require
+require('dotenv').config()
+
+
+// Create Neo4j driver instance
+const driver = neo4j.driver(
+    process.env.AURA_ENDPOINT,
+  neo4j.auth.basic(
+    process.env.AURA_USERNAME,
+    process.env.AURA_PASSWORD
+  )
+);
+
+const typeDefs = gql`
+  type Person {
+    name: String
+    knows: [Person!]! @relationship(type: "KNOWS", direction: OUT)
+    friendCount: Int
+      @cypher(statement: "RETURN SIZE((this)-[:KNOWS]->(:Person))")
+  }
+`;
+
+// Create instance that contains executable GraphQL schema from GraphQL type definitions
+const neo4jGraphQL = new Neo4jGraphQL({
+  typeDefs,
+  driver,
+});
+
+// Generate schema
+neo4jGraphQL.getSchema().then((schema) => {
+  // Create ApolloServer instance to serve GraphQL schema
+  const server = new ApolloServer({
+    schema,
+    context: { driverConfig: { database: "neo4j" } },
+    //introspection: true,
+    //playground: true,
+  });
+
+  // Start ApolloServer
+  server.listen().then(({ url }) => {
+    console.log(`GraphQL server ready at ${url}`);
+  });
+});
